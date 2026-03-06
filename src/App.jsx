81791e650fac46bbf5167ac8912ce8733832f67e
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 12:21:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import AppRouter from "./router/AppRouter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default function App() {
+import Home from "./pages/Home";
+import WorkingCapital from "./pages/WorkingCapital";
+import Agriculture from "./pages/Agriculture";
+import Banking from "./pages/Banking";
+import Dashboard from "./pages/Dashboard";
+import Underwriting from "./pages/Underwriting";
+import FinalReport from "./pages/FinalReport";
+
+function App() {
   return (
-    <div className="app-container">
-      <AppRouter />
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/working-capital" element={<WorkingCapital />} />
+
+        <Route path="/agriculture" element={<Agriculture />} />
+
+        <Route path="/banking" element={<Banking />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/underwriting" element={<Underwriting />} />
+
+        <Route path="/final-report" element={<FinalReport />} />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
+
+export default App;
