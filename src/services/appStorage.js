c65e3ab3766f0ac9d861e7f6ac078b5e0da8b38c
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 09:43:03 +0530

    Update appStorage.js

--- src/services/appStorage.js
+++ src/services/appStorage.js
@@ -1,1 +1,23 @@
+const KEY="credit_app_v1";
 
+export function loadApp(){
+
+return JSON.parse(localStorage.getItem(KEY)) || {};
+
+}
+
+export function saveApp(data){
+
+localStorage.setItem(KEY,JSON.stringify(data));
+
+}
+
+export function updateModule(module,data){
+
+const app=loadApp();
+
+app[module]=data;
+
+saveApp(app);
+
+}
