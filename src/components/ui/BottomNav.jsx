3b244d14660ba334dc795f400a17955ffe7f56fa
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 12:39:18 +0530

    Update BottomNav.jsx

--- src/components/ui/BottomNav.jsx
+++ src/components/ui/BottomNav.jsx
@@ -1,24 +1,79 @@
-import { Link } from "react-router-dom";
-import { Home, BarChart3, Leaf, Landmark, FileText } from "lucide-react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-export default function BottomNav(){
+import Home from "./pages/Home";
+import WorkingCapital from "./pages/WorkingCapital";
+import Agriculture from "./pages/Agriculture";
+import Banking from "./pages/Banking";
+import Dashboard from "./pages/Dashboard";
+import Underwriting from "./pages/Underwriting";
+import FinalReport from "./pages/FinalReport";
+
+import { Home as HomeIcon, BarChart3, Leaf, Landmark, FileText } from "lucide-react";
+
+
+function BottomNav(){
 
 return(
 
 <div className="fixed bottom-0 left-0 right-0 bg-slate-900 flex justify-around p-3 text-white">
 
-<Link to="/"><Home size={20}/></Link>
+<Link to="/">
+<HomeIcon size={22}/>
+</Link>
 
-<Link to="/working-capital"><BarChart3 size={20}/></Link>
+<Link to="/working-capital">
+<BarChart3 size={22}/>
+</Link>
 
-<Link to="/agriculture"><Leaf size={20}/></Link>
+<Link to="/agriculture">
+<Leaf size={22}/>
+</Link>
 
-<Link to="/banking"><Landmark size={20}/></Link>
+<Link to="/banking">
+<Landmark size={22}/>
+</Link>
 
-<Link to="/final-report"><FileText size={20}/></Link>
+<Link to="/final-report">
+<FileText size={22}/>
+</Link>
 
 </div>
 
 )
 
 }
+
+
+function App(){
+
+return(
+
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<Home/>} />
+
+<Route path="/working-capital" element={<WorkingCapital/>} />
+
+<Route path="/agriculture" element={<Agriculture/>} />
+
+<Route path="/banking" element={<Banking/>} />
+
+<Route path="/dashboard" element={<Dashboard/>} />
+
+<Route path="/underwriting" element={<Underwriting/>} />
+
+<Route path="/final-report" element={<FinalReport/>} />
+
+</Routes>
+
+<BottomNav/>
+
+</BrowserRouter>
+
+)
+
+}
+
+export default App;
