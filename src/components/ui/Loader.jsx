93aa0b80af6a42fdd7629c6978694e622dea5ec8
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 13:04:46 +0530

    Update and rename Loader.jsx to Loading.jsx

--- src/components/ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-export default function Loader(){
-
-return(
-
-<p>Loading...</p>
-
-)
-
-}
---
+++ src/components/ui/Loading.jsx
@@ -0,0 +1,19 @@
+import { ThreeDots } from "react-loader-spinner";
+
+export default function Loading(){
+
+return(
+
+<div className="flex justify-center items-center h-screen">
+
+<ThreeDots
+height="80"
+width="80"
+color="#22c55e"
+/>
+
+</div>
+
+)
+
+}
