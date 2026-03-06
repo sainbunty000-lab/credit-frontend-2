c46e03f5dd223f261538362395b4e6760bf44161
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-06 12:34:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,30 @@ import Banking from "./pages/Banking";
 import Dashboard from "./pages/Dashboard";
 import Underwriting from "./pages/Underwriting";
 import FinalReport from "./pages/FinalReport";
+import { Link } from "react-router-dom";
+import { Home, BarChart3, Leaf, Landmark, FileText } from "lucide-react";
 
+export default function BottomNav(){
+
+return(
+
+<div className="fixed bottom-0 left-0 right-0 bg-slate-900 flex justify-around p-3 text-white">
+
+<Link to="/"><Home size={20}/></Link>
+
+<Link to="/working-capital"><BarChart3 size={20}/></Link>
+
+<Link to="/agriculture"><Leaf size={20}/></Link>
+
+<Link to="/banking"><Landmark size={20}/></Link>
+
+<Link to="/final-report"><FileText size={20}/></Link>
+
+</div>
+
+)
+
+}
 function App() {
   return (
     <BrowserRouter>
