52c54e2829f1c5248108942885ef21a67dc815de
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 20:29:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import WorkingCapital from "./pages/WorkingCapital";
 import Agriculture from "./pages/Agriculture";
 import Banking from "./pages/Banking";
 import FinalReport from "./pages/FinalReport";
+import SavedReports from "./pages/SavedReports";
 
 import BottomNav from "./components/BottomNav";
 
@@ -27,6 +28,10 @@ return(
 <Route path="/banking" element={<Banking/>} />
 
 <Route path="/final-report" element={<FinalReport/>} />
+
+{/* SAVED CAM REPORTS */}
+
+<Route path="/saved" element={<SavedReports/>} />
 
 </Routes>
 
