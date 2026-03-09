81ea34687f4c529d91cd41d3878612f9a65612b8
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 22:23:42 +0530

    Update WorkingCapital.jsx

--- src/pages/WorkingCapital.jsx
+++ src/pages/WorkingCapital.jsx
@@ -200,7 +200,7 @@ export default function WorkingCapital() {
 
   return (
 
-<div className="min-h-screen bg-[#070b14] p-4 sm:p-6 pt-20 pb-24 text-slate-200">
+<div className="min-h-screen bg-[#070b14] p-4 sm:p-6 pt-20 pb-32 text-slate-200">
 
       <NavigationButtons prev="/" next="/agriculture" />
 
