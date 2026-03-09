74de99f3d0265108e7c01d70e5419d58156b30ba
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 22:20:31 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,11 +5,11 @@ export default function BottomNav(){
 
 return(
 
-<div className="fixed bottom-0 left-0 right-0 bg-slate-900 flex justify-center p-3 text-white border-t border-slate-800 z-50">
+<div className="fixed bottom-6 left-0 right-0 flex justify-center z-50">
 
 <Link
 to="/"
-className="flex items-center gap-2 bg-slate-800 px-6 py-2 rounded-lg hover:bg-slate-700 transition"
+className="flex items-center gap-2 bg-slate-800 px-6 py-2 rounded-lg text-white shadow-lg"
 >
 
 <Home size={20}/>
