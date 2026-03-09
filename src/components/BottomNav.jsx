8ce3c07156bf1a3ad561b01e60064cf62cd9f056
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 17:16:50 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
-import { Home as HomeIcon, BarChart3, Leaf, Landmark, FileText } from "lucide-react";
+import { Home, BarChart3, Leaf, Landmark, FileText } from "lucide-react";
 
-function BottomNav(){
+export default function BottomNav(){
 
 return(
 
 <div className="fixed bottom-0 left-0 right-0 bg-slate-900 flex justify-around p-3 text-white">
 
-<Link to="/"><HomeIcon size={22}/></Link>
+<Link to="/"><Home size={22}/></Link>
+
 <Link to="/working-capital"><BarChart3 size={22}/></Link>
+
 <Link to="/agriculture"><Leaf size={22}/></Link>
+
 <Link to="/banking"><Landmark size={22}/></Link>
+
 <Link to="/final-report"><FileText size={22}/></Link>
 
 </div>
@@ -18,5 +22,3 @@ return(
 )
 
 }
-
-export default BottomNav;
