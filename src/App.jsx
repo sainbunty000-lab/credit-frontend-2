f5df556c6c080bfe9e4028a2d5c9e3ee95cd47d2
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 16:28:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import WorkingCapital from "./pages/WorkingCapital";
@@ -6,70 +6,36 @@ import Agriculture from "./pages/Agriculture";
 import Banking from "./pages/Banking";
 import FinalReport from "./pages/FinalReport";
 
-import {
-  Home as HomeIcon,
-  BarChart3,
-  Leaf,
-  Landmark,
-  FileText
-} from "lucide-react";
+import BottomNav from "./components/BottomNav";
 
-/* Bottom Navigation */
+export default function App(){
 
-function BottomNav() {
-  return (
-    <div className="fixed bottom-0 left-0 right-0 bg-slate-900 flex justify-around p-3 text-white border-t border-slate-700">
+return(
 
-      <Link to="/">
-        <HomeIcon size={20}/>
-      </Link>
+<BrowserRouter>
 
-      <Link to="/working-capital">
-        <BarChart3 size={20}/>
-      </Link>
+<div className="pb-16">
 
-      <Link to="/agriculture">
-        <Leaf size={20}/>
-      </Link>
+<Routes>
 
-      <Link to="/banking">
-        <Landmark size={20}/>
-      </Link>
+<Route path="/" element={<Home/>} />
 
-      <Link to="/final-report">
-        <FileText size={20}/>
-      </Link>
+<Route path="/working-capital" element={<WorkingCapital/>} />
 
-    </div>
-  );
-}
+<Route path="/agriculture" element={<Agriculture/>} />
 
-/* Main App */
+<Route path="/banking" element={<Banking/>} />
 
-export default function App() {
-  return (
-    <BrowserRouter>
+<Route path="/final-report" element={<FinalReport/>} />
 
-      <div className="pb-16">
+</Routes>
 
-        <Routes>
+</div>
 
-          <Route path="/" element={<Home />} />
+<BottomNav/>
 
-          <Route path="/working-capital" element={<WorkingCapital />} />
+</BrowserRouter>
 
-          <Route path="/agriculture" element={<Agriculture />} />
+)
 
-          <Route path="/banking" element={<Banking />} />
-
-          <Route path="/final-report" element={<FinalReport />} />
-
-        </Routes>
-
-      </div>
-
-      <BottomNav/>
-
-    </BrowserRouter>
-  );
 }
