d6e93b6091a8f571e638d540b7ba2689e885d02f
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-09 17:16:18 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,55 +2,54 @@ const API = "https://credit-backend-production-d988.up.railway.app";
 
 /* AGRICULTURE */
 
-export const agriCalculate = async (data) => {
+export const agriCalculate = async (data)=>{
 
-  const res = await fetch(`${API}/agriculture/calculate`,{
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json"
-    },
-    body:JSON.stringify(data)
-  });
+const res = await fetch(`${API}/agriculture/calculate`,{
+method:"POST",
+headers:{ "Content-Type":"application/json" },
+body:JSON.stringify(data)
+});
 
-  return res.json();
+return res.json();
+
 };
 
 
 /* BANKING */
 
-export const bankingAnalyze = async (formData) => {
+export const bankingAnalyze = async (formData)=>{
 
-  const res = await fetch(`${API}/banking/full-analysis`,{
-    method:"POST",
-    body:formData
-  });
+const res = await fetch(`${API}/banking/full-analysis`,{
+method:"POST",
+body:formData
+});
 
-  return res.json();
+return res.json();
+
 };
 
 
 /* WORKING CAPITAL */
 
-export const wcUploadDual = async (formData) => {
+export const wcUploadDual = async (formData)=>{
 
-  const res = await fetch(`${API}/wc/upload-dual`,{
-    method:"POST",
-    body:formData
-  });
+const res = await fetch(`${API}/wc/upload-dual`,{
+method:"POST",
+body:formData
+});
 
-  return res.json();
+return res.json();
+
 };
 
+export const wcManualCalc = async (data)=>{
 
-export const wcManualCalc = async (data) => {
+const res = await fetch(`${API}/wc/manual-calc`,{
+method:"POST",
+headers:{ "Content-Type":"application/json" },
+body:JSON.stringify(data)
+});
 
-  const res = await fetch(`${API}/wc/manual-calc`,{
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json"
-    },
-    body:JSON.stringify(data)
-  });
+return res.json();
 
-  return res.json();
 };
