b0ef35c4476959cea6aea3c085fa41a177d4649d
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-10 08:46:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
+
+/* GLOBAL STYLES */
+
 import "./index.css";
+import "./styles/designSystem.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
 <React.StrictMode>
+
 <App />
+
 </React.StrictMode>
+
 );
