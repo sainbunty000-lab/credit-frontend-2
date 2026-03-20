549cf4c505b9f62f08f62933df9122cddad6b4b7
sainbunty000-lab <sainbunty000@gmail.com>
2026-03-20 09:05:36 +0530

    Delete src directory

--- src/calculateRatios.js
+++
@@ -1,9 +0,0 @@
-export function calculateRatios(data) {
-  const bs = data.balance_sheet;
-  const pnl = data.pnl;
-
-  return {
-    current_ratio: bs.current_assets / bs.current_liabilities || 0,
-    net_margin: pnl.net_profit / pnl.revenue || 0
-  };
-}
--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE = "https://your-app.onrender.com";
